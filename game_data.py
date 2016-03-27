94710ab6852594d65d8f3444457a09697d3d12e6
Seth Creech <sacreech@moreheadstate.edu>
2016-03-27 03:36:35 -0400

    Added some functions for GiantBomb's API

--- game_data.py
+++ game_data.py
@@ -1,7 +1,23 @@
 from twitch.apis import games, streams
+import giantbomb.apis.game 
+
+
+cached_release_dates = {}
 
 def getTopGames(**params):
 	return games.top_games(**params)['top']
 
 def getGameStreams(game, **params):
 	return streams.list(game=game, **params)["streams"]
+	
+def getGiantBombID(game):
+	return game["game"]["giantbomb_id"]
+	
+def getReleaseDate(id):
+	
+	if id in cached_release_dates:
+		return cached_release_dates[id]
+	
+	game_release_date = giantbomb.apis.game.release_date(id)
+	cached_release_dates[id] = game_release_date
+	return game_release_date
